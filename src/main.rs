b886159ff4f25b0476c9536391c8f1f097dc52a2
Eeviika <84326942+Eeviika@users.noreply.github.com>
2025-11-18 18:07:07 -0500

    Added basic Macroquad hello world window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("Hello World")]
+async fn main() {
+    loop {
+        clear_background(WHITE);
+        draw_text("Hello, world!", 20.0, 20.0, 30.0, BLACK);
+        next_frame().await;
+    }
 }
