73676f4935ec3034a25f99b3260c18fb60c7c97b
Eeviika <84326942+Eeviika@users.noreply.github.com>
2025-11-28 18:49:52 -0500

    Define DialogElement enum and update Dialog struct with dialogue vector

--- src/dialog.rs
+++ src/dialog.rs
@@ -1,5 +1,17 @@
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum DialogElement {
+    Dialog { text: String },
+    PageBreak,
+}
 
-
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Dialog {
+    name: String,
+    dialogue: Vec<DialogElement>,
+}
 
+impl Default for Dialog {
+    fn default() -> Self {
+        Dialog { name: "dialogue".to_string(), dialogue: vec![DialogElement::Dialog { text: "Hello, World!".to_string() }] }
+    }
 }
