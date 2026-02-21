bd5f9cf0f0fbc55a7770a4a9107e83f6827cbb32
Eeviika <84326942+Eeviika@users.noreply.github.com>
2026-02-21 15:28:35 -0500

    added doc comments

--- src/entities.rs
+++ src/entities.rs
@@ -1,5 +1,36 @@
 use crate::game::{ColorType, DialogId};
 
+/// Data for an 8x8 texture.
+/// 
+/// `TileData` represents a small, 8x8, boolean pixel texture that can be
+/// used by tiles, NPCs, and other engine elements. Each pixel is stored as a
+/// `bool`, allowing what is as close to one bit per pixel as can be in standard Rust. 
+/// The data can contain up to **two frames** of animation. Most things use this struct.
+/// 
+/// ## Fields
+/// 
+/// - `frames` - A `[[[bool; 8]; 8]; 2]` array that is indexed by frame;row;col.
+/// - `color_type` - A `ColorType` enum that determines how the renderer will color this texture.
+/// 
+/// ## Example
+///
+/// ```ignore
+/// use crate::entities::TileData;
+/// use crate::game::ColorType;
+///
+/// let default_data = TileData::default();
+///
+/// let custom_data = TileData {
+///     frames: [[[false; 8]; 8]; 2],
+///     color_type: ColorType::Tile,
+/// };
+/// ```
+/// 
+/// ## Notes
+/// 
+/// `TileData` is inexpensive to create, but writing out full frame data in code can be tedious and unnecessary.
+/// Alternative means of creation are encouraged, such as using the in-engine editor.
+/// (Or just don't...)
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct TileData {
     frames: [[[bool; 8]; 8]; 2],
@@ -27,7 +58,7 @@ pub struct Avatar {
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Npc {
     pub entity: Entity,
-    pub interact_sound_id: Option<u32>,
+    pub interact_sound_id: Option<u32>, // todo: make a SoundId type
     pub dialog_id: Option<DialogId>,
     pub enabled: bool,
 }
